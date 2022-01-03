c04f7b9393344cb204a7ac77f1171c022fc29266
Gustavo Coelho <tauronestudios@outlook.com>
2022-01-02 21:06:37 -0300

    Initial commit

---
+++ Source/ArkanoidMentorama/ArkanoidMentoramaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArkanoidMentoramaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOIDMENTORAMA_API AArkanoidMentoramaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ArkanoidMentorama/Public/ArkanoidPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ArkanoidPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOIDMENTORAMA_API AArkanoidPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
